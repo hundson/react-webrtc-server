9517b01ae2f12d831d14e180d204c91d03c3ff3c
Bill <imanaibot@gmail.com>
2023-08-23 13:13:50 +0800

    change commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { v4: uuidv4 } = require("uuid");
 const cors = require("cors");
 const cron = require("node-cron");
 
-cron.schedule("14 * * * *", () => {
+cron.schedule("10 * * * * *", () => {
   console.log("");
 });
 
